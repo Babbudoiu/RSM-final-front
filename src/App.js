d8b97f88d84dcc3973088795165fd6c83f85c365
Nick Bilsborrow <nickbilsborrow15@gmail.com>
2021-08-10 12:02:42 +0100

    push for the sack of full merge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Navbar from './components/Navbar';
+import { AdminLogin } from "./pages/AdminLogin";
 
 const App  = () => {
   const [admin, setAdmin] = useState();
-  const[user,setUser]=  useState(initialState)
+  const[user, setUser]=  useState();
   return(
     <Router>
-      <Navbar /> 
+      
+     
+      {!admin ? <Redirect to="/" /> : !user ? <Redirect to="/userLogin" /> : <Redirect to="/home" /> }
+
+
+      <Route exact path="/">
+        <AdminLogin setAdmin={setAdmin} />
+      </Route>
+
+      <Route exact path="/userLogin">
+        <userLogin setuser={setUser} />
+      </Route>
+
+
     </Router>
   )
 };
---
+++ src/pages/AdminLogin/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { FormContainer, LandingContainer, LogButton, LogForm, LogInput } from "../../styledComponents";
+
+import { fetchAdmin } from "../../utils";
+
+
+
+export const AdminLogin = ({setAdmin  }) => {
+    const [adminUsername, setAdminUsername] = useState();
+    const [adminPassword, setAdminPassword] = useState();
+  return (
+    <LandingContainer>
+      
+      <FormContainer>
+        <LogForm
+          onSubmit={(e) => fetchAdmin(e, adminUsername, adminPassword, setAdmin)}
+        >
+          
+          <LogInput
+            onChange={(e) => setAdminUsername(e.target.value)}
+            placeholder="Admin Username"
+          />
+          <LogInput type="password"
+            onChange={(e) => setAdminPassword(e.target.value)}
+            placeholder="Admin Password"></LogInput>
+          <LogButton type="submit">Admin Login</LogButton>
+          
+          
+        
+        </LogForm>
+      </FormContainer>
+    </LandingContainer>
+  );
+};
