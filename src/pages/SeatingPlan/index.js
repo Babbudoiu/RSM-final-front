04e74e8daa68292571985ead826e51acf57893b3
Gaby Hall <gabyhall@Jonathan-Robertss-iMac.local>
2021-08-11 16:37:24 +0100

    draft tables

--- src/pages/SeatingPlan/index.js
+++ src/pages/SeatingPlan/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import Tables from '../../components/Tables'
 
 const SeatingPlan = ({setUser}) => {
     return (
         <>
         <Navbar setUser={setUser}/>
+        <Tables />
         <Footer />
         </>
     )
