dd008d830a632ef81f55e572358e8ac0c3187094
Jabar Hassan <jabarhassan@jabars-mbp.home>
2021-08-11 09:59:24 +0100

    side bar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { SidebarContainer, Icon, CloseIcon, SidebarMenu, SideBtnWrap, SidebarRoute, SidebarLink, SidebarWrapper} from './SidebarElements';
+
+
+
+const Sidebar = (isOpen, toggle) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle} >
+            <Icon onClick={toggle}>
+                <CloseIcon />
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="reservations" onClick={toggle}>Reservations</SidebarLink>
+                    <SidebarLink to="seating-plan" onClick={toggle}>Seating Plan</SidebarLink>
+                    <SidebarLink to="menu" onClick={toggle}>Menu</SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to="/signout">Sign Out</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar;
