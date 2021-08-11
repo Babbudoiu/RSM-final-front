87cbc84228abea1f98938ff526eb39fffb6a3935
Mattia Biscontini <mattiabiscontini@gmail.com>
2021-08-11 13:01:08 +0100

    bool switch

--- src/components/MealTimes/index.js
+++ src/components/MealTimes/index.js
@@ -2,18 +2,24 @@ import React, { useState } from 'react';
 
 export const MealTimes = ({showMeals}) => {
 
-const [lunch, setLunch] = useState();
-const [dinner, setDinner] = useState();
+const [lunch, setLunch] = useState(false);
+const [dinner, setDinner] = useState(false);
 
 
     return (
        <div className="meals" style={showMeals ? {} : {display: 'none'}}>  
            <p>Lunch or Dinner?</p>
-           <button onClick={() => {setLunch(true)}}>Open Lunch Tab</button>
-           <button onClick={() => {setLunch(false)}}>Close Lunch Tab</button>
-           <button onClick={() => {setDinner(true)}}>Open Dinner Tab</button>
-           <button onClick={() => {setDinner(false)}}>Close Dinner Tab</button>
-        </div> 
+           <button onClick={() => {
+               if (lunch === true) {
+               setLunch(false)
+           } else if(lunch === false) {
+               setLunch(true)}; console.log(lunch)}}>Lunch</button>
+           <button onClick={() => {
+               if (dinner === true) {
+                setDinner(false)
+            } else if(dinner === false) {
+                setDinner(true)}; console.log(dinner)}}>Dinner</button>
+           </div> 
        
     )
     }
