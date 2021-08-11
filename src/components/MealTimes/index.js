07e47407fe692b25da9bf0c7ff4f1b9783f27e39
Mattia Biscontini <mattiabiscontini@gmail.com>
2021-08-11 13:34:59 +0100

    better func for bool

--- src/components/MealTimes/index.js
+++ src/components/MealTimes/index.js
@@ -2,26 +2,33 @@ import React, { useState } from 'react';
 
 export const MealTimes = ({showMeals}) => {
 
-const [lunch, setLunch] = useState(false);
-const [dinner, setDinner] = useState(false);
+const [lunch, setLunch] = useState(true);
+const [dinner, setDinner] = useState(true);
 
+const lunchHandler = () => {
+    if (lunch === true) {
+        setLunch(false)
+    } else if(lunch === false) {
+        setLunch(true)}
+         console.log(lunch)
+    }
+
+const dinnerHandler = () => {
+    if (dinner === true) {
+        setDinner(false)
+    } else if(dinner === false) {
+        setDinner(true)}
+        console.log(dinner)
+    }
 
     return (
        <div className="meals" style={showMeals ? {} : {display: 'none'}}>  
            <p>Lunch or Dinner?</p>
-           <button onClick={() => {
-               if (lunch === true) {
-               setLunch(false)
-           } else if(lunch === false) {
-               setLunch(true)}; console.log(lunch)}}>Lunch</button>
-           <button onClick={() => {
-               if (dinner === true) {
-                setDinner(false)
-            } else if(dinner === false) {
-                setDinner(true)}; console.log(dinner)}}>Dinner</button>
+           <button onClick={lunchHandler}>Lunch</button>
+           <button onClick={dinnerHandler}>Dinner</button>
            </div> 
        
     )
-    }
+}
 
-    export default MealTimes;
+export default MealTimes;
